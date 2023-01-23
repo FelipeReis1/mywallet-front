a8f1ef157440795b5d8c430c5fa33e55f4e294f5
FelipeReis1 <feliperg15@yahoo.com.br>
2023-01-22 23:31:18 -0300

    partial OutcomePage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginPage from "../src/components/LoginPage";
 import RegistrationPage from "./components/RegistrationPage";
 import IncomePage from "./components/IncomePage";
 import HomePage from "./components/HomePage";
+import OutcomePage from "./components/OutcomePage";
 export default function App() {
   const [user, setUser] = useState({});
   const contextValue = { user, setUser };
@@ -16,9 +17,7 @@ export default function App() {
           <Route path="/cadastro" element={<RegistrationPage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/nova-entrada" element={<IncomePage />} />
-          {/*
-              
-              <Route path="/nova-saida" element={< />} /> */}
+          <Route path="/nova-saida" element={<OutcomePage />} />
         </Routes>
       </BrowserRouter>
     </UserContext.Provider>
